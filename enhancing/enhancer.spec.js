5c7133fc0c277801e1b6f33469fbcde7fbf59dee
SRattacasa <38533+SRattacasa@users.noreply.github.com>
2020-09-09 15:26:11 -0400

    MVP

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,44 @@
 const enhancer = require('./enhancer.js');
+
 // test away!
+
+
+    it("Has been repaired", () => { 
+        expect(enhancer.repair({
+            name: "Hat",
+            durability: 75,
+            enhancement: 18,
+        }))
+        .toEqual({
+            name: "Hat",
+            durability: 100,
+            enhancement: 18,
+        })
+    })
+
+    it("Has been succeeded", () => { 
+        expect(enhancer.success({
+            name: "Hat",
+            durability: 75,
+            enhancement: 18,
+        }))
+        .toEqual({
+            name: "Hat",
+            durability: 75,
+            enhancement: 19,
+        })
+    })
+
+    it("Has been failed", () => { 
+        expect(enhancer.fail({
+            name: "Hat",
+            durability: 75,
+            enhancement: 18,
+        }))
+        .toEqual({
+            name: "Hat",
+            durability: 65,
+            enhancement: 17,
+        })
+    })
+ 
